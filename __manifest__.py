3bac9632dba53fd37cc4b897af15cc3e2fa2642f
kubangab <142019179+kubangab@users.noreply.github.com>
2024-07-08 22:09:34 +0200

    Update __manifest__.py
    Updated to new version numbering

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Custom Invoice Header',
-    'version': '16.0.1.0.2',
+    'version': '1.2.0',
     'category': 'Accounting',
     'summary': 'Customize invoice header to show invoice number from page 2 onwards',
     'description': """
